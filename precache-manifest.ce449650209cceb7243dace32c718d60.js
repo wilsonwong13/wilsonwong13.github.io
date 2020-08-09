1d6806956b0051434e04056d87df35539ec3d32e
Wilson Wong <wilson.wong@ibm.com>
2020-08-09 13:59:43 -0400

    Updates

---
+++ precache-manifest.ce449650209cceb7243dace32c718d60.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f74d9174ec6c4664706cfe3bd75bf95",
+    "url": "/developerfolio/index.html"
+  },
+  {
+    "revision": "12fcd9e06e6ae0a8da41",
+    "url": "/developerfolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "abf2fd62e5967249a93e",
+    "url": "/developerfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "597231f4a0b2883269f3",
+    "url": "/developerfolio/static/css/main.1dcbe4b2.chunk.css"
+  },
+  {
+    "revision": "14054666595e41501607",
+    "url": "/developerfolio/static/js/2.740b40b1.chunk.js"
+  },
+  {
+    "revision": "12fcd9e06e6ae0a8da41",
+    "url": "/developerfolio/static/js/3.123a7c76.chunk.js"
+  },
+  {
+    "revision": "abf2fd62e5967249a93e",
+    "url": "/developerfolio/static/js/4.c490c9dd.chunk.js"
+  },
+  {
+    "revision": "597231f4a0b2883269f3",
+    "url": "/developerfolio/static/js/main.9c1488a7.chunk.js"
+  },
+  {
+    "revision": "aa1c9abfeb7ffe88ac8b",
+    "url": "/developerfolio/static/js/runtime-main.426a2930.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3d8dcb1fe63fef85334f1c7bdfdd2c19",
+    "url": "/developerfolio/static/media/cert_admin_rgb.3d8dcb1f.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "ae1553bd3258c08d17741974906c8ba7",
+    "url": "/developerfolio/static/media/service-cloud-cert.ae1553bd.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/developerfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
